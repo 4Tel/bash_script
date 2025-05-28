45d4dd489dbf06847f310c2100783714c26034af
4Tel <kyako95@gmail.com>
2025-05-28 20:39:10 +0900

    .

--- script/ssh/user.sh
+++ script/ssh/user.sh
@@ -5,8 +5,8 @@ echo "User: $USER"
 if [ -z "$SSH_CONNECTION" ]; then
   echo "SSH_CONNECTION is not defined."
 else
-  echo "Client IP: $(echo "$SSH_CONNECTION" | awk '{print $1}')"
+  echo "Client IP  : $(echo "$SSH_CONNECTION" | awk '{print $1}')"
   echo "Client Port: $(echo "$SSH_CONNECTION" | awk '{print $2}')"
-  echo "Server IP: $(echo "$SSH_CONNECTION" | awk '{print $3}')"
+  echo "Server IP  : $(echo "$SSH_CONNECTION" | awk '{print $3}')"
   echo "Server Port: $(echo "$SSH_CONNECTION" | awk '{print $4}')"
 fi
